4c40f993f3bb8486de4d1e9ecc5840011cd3add0
anish1840 <anishsingh7379@gmail.com>
2025-04-06 10:37:39 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=1234567
+let accountEmail="anish@google.com"
+var accountpasswords="129867"
+accountCity="jaipur"
+let accountstate
+//accountId=0000111
+accountEmail="ravi@google.com"
+accountpasswords="444555"
+accountCity="delhi"
+console.table([accountId, accountEmail,accountpasswords,accountCity,accountstate])
+/*
+Prefer not use to var
+bcause of issue in block scope & functional scope
+*/
