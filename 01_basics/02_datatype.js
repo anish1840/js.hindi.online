63808dafdfe6bba2192d84271d26c4c84a2ce9fe
anish1840 <anishsingh7379@gmail.com>
2025-04-06 10:37:01 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  //  we are using nodejs/not browser
+
+console.log(3
+    +
+    3
+)     //code readability should be high
+
+console.log("Anish")
+
+let name="abhishek"
+let age=18
+let logedIn=true
+let state;
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//Null=standalone value
+//undifined=>
+//symbol =>unique 
+
+
+//object
+
+console.log(typeof "amit")
+console.log(typeof null) //object
+console.log(typeof undefined) //undifined
