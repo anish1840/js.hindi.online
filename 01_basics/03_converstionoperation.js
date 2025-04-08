d0478802e8fd8258a6733743841d28b11174bd63
anish1840 <anishsingh7379@gmail.com>
2025-04-08 08:03:52 +0000

    convertion of data type

---
+++ 01_basics/03_converstionoperation.js
@@ -0,0 +1,27 @@
+let score="33"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN/Not a Number
+//"true" => 1; "false"=> 0;
+
+//let isLoggedIn= 1
+let isLoggedIn= ""
+
+let booleanIsLoggedIN=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIN)
+
+// 1=>true ,0=>false
+// ""=>false
+//"ravi"=>true
+
+let someNumber=32
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
